ff439b9ab0512d03b57541a91a384603f839b36b
Nikhil <nikhilpawar982001@gmail.com>
2021-05-19 15:00:45 +0530

    Initial Commit

---
+++ projbackend/models/category.js
@@ -0,0 +1,17 @@
+const { trim } = require("lodash")
+const mongoose = require("mongoose")
+
+const categorySchema = new mongoose.Schema(
+    {
+    name:{
+        type: String,
+        trim : true,
+        required : true,
+        maxlength : 32,
+        unique: true
+    }
+}, {timestamps: true});
+
+
+
+module.exports = mongoose.model("Category", categorySchema)
---
+++ projbackend/models/user.js
@@ -0,0 +1,82 @@
+const { update } = require("lodash");
+var mongoose = require("mongoose");
+const crypto = require("crypto");
+import { time } from "console";
+import { v4 as uuidv4 } from "uuid";
+
+var userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 32,
+      trim: true,
+    },
+
+    lastname: {
+      type: String,
+      maxlength: 32,
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+
+    userinfo: {
+      type: String,
+      trim: true,
+    },
+    //TODO: come back here
+    encry_password: {
+      type: String,
+      required: true,
+    },
+
+    salt: String,
+
+    role: {
+      type: Number,
+      default: 0,
+    },
+    purchases: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv4();
+    this.encry_password = this.securePassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.method = {
+  authenticate: function (plainpassword) {
+    return this.securePassword(plainpassword) === this.encry_password;
+  },
+  securePassword: function (plainpassword) {
+    if (!password) {
+      return "";
+      try {
+        return crypto
+          .createHmac("sha256", this.salt)
+          .update(plainpassword)
+          .digest("hex");
+      } catch (error) {
+        return "";
+      }
+    }
+  },
+};
+
+module.exports = mongoose.model("User", userSchema);
