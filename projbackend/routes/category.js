97a7d2c0816555dd7904a56a8f2a2f3852b7babd
Nikhil <nikhilpawar982001@gmail.com>
2021-06-21 20:20:42 +0530

    9.3

--- projbackend/routes/category.js
+++ projbackend/routes/category.js
@@ -1,11 +1,16 @@
 const express = require("express")
 const router = express.Router()
 
-const {getCategoryById} = require("../controllers/category")
+const {getCategoryById, createCategory} = require("../controllers/category")
 const {isAdmin, isSignedIn, isAuthenticated} = require("../controllers/auth")
 const {getUserById} = require("../controllers/user")
 
+
+//params
 router.param("userId", getUserById);
 router.param("categoryId", getCategoryById);
+
+//actual rotues goes here
+router.post("/category/create/userId", isSignedIn,  isAuthenticated, isAdmin,createCategory)
 
 module.exports = router;
