20d83dd22e75a802f2a1d5b1381cc2538b0ffe6c
Nikhil <nikhilpawar982001@gmail.com>
2021-06-25 15:18:37 +0530

    10.8

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -72,9 +72,70 @@ exports.getProduct = (req,res)=>{
     return res.json(req.product)
 };
 
+//middleware
 exports.photo = (req,res, next)=>{
     if (req.product.photo.data) {
         res.set("Content-Type", req.product.photo.contetType)
         return res.send(req.product.photo.data);
     }
+}
+
+
+//delete controllers
+exports.deleteProduct =  (req,res)=>{
+    let product = req.product;
+    product.remove((err, deletedProduct)=>{
+        if (err) {
+            return res.status(400).json({
+                error: "Failed to delete product"
+            })
+        }
+        res.json({
+            message:"Deletion was succes",
+            deletedProduct
+        })
+    })
+}
+
+//update controllers
+exports.updateProduct =  (req,res)=>{
+    let form  = new formidable.IncomingForm();
+    formidable.keepExtensions = true;
+
+    form.parse(req, (err, fields, file) =>{
+        if (err) {
+            return res.status(400).json({
+                eroor: "Problem with image"
+            })
+        };
+
+        //destructure the fields
+        const {name, description,price, category, stock,} = fields;
+        
+        //updation code
+        let product = req.product;
+        product = _.extend(product, fields)
+
+        //handle file here 
+        if (file.photo) {
+            if (file.photo.size >3000000) {
+                return res.status(400).json({
+                    error: "File size too bog!"
+                })
+            }
+            product.photo.data = fs.readFileSync(file.photo.path);
+            product.photo.contetType = file.photo.type;
+        };
+
+        //save to DB
+        product.save((err,product) => {
+            if (err) {
+                res.status(400).json({
+                    error: "Updating tshirt in DB failed"
+                })
+            }
+            res.json(product)
+
+        })
+    });
 }
