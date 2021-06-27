fca388b1c8ab15944a83ce63d4ef5f7fcf2975ad
Nikhil <nikhilpawar982001@gmail.com>
2021-06-27 19:30:52 +0530

    11.4

--- projbackend/routes/order.js
+++ projbackend/routes/order.js
@@ -6,4 +6,31 @@ const{isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth")
 const{getUserById, pushOrderInPurchaseList} = require("../controllers/user")
 const {updateStock} = require("../controllers/product")
 
-const {} = require("../controllers/order")
+const {getOrderById, createOrder,getAllOrders} = require("../controllers/order")
+
+///params
+router.param("userId", getUserById)
+router.param("orderId", getOrderById)
+
+
+//actual routes
+//create
+router.post("order/create/:userId", 
+    isSignedIn,
+    isAuthenticated, 
+    pushOrderInPurchaseList,
+    updateStock, 
+    createOrder
+);
+
+//read
+router.get("order/all/:userId",
+isSignedIn,
+isAuthenticated,
+isAdmin,
+getAllOrders
+
+)
+
+
+module.exports = router;
