f40e157c39069ceb32b4bfad7442905348c9067d
Nikhil <nikhilpawar982001@gmail.com>
2021-07-01 22:53:29 +0530

    13.4

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "../styles.css"
 
 export default function Home() {
     return (
         <div>
-            <h1>Hello frontend</h1>
+            <h1 className="text-white">Hello frontend</h1>
         </div>
     )
 }
