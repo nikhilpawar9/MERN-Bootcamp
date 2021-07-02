b518169c3060d69a555064ee5f32d1aa2c39966a
Nikhil <nikhilpawar982001@gmail.com>
2021-07-02 13:17:04 +0530

    13.6

---
+++ projfrontend/src/core/Base.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Base = ({
+    title = " My title",
+    description = " my description",
+    className = "bg-dark text-white p-4",
+    children
+}) => 
+    (
+        <div>
+            <div className="container-fluid">
+                <div className="jummbotron bg-dark text-white text-center">
+                    <h2 className="display-4">{title}</h2>
+                    <p className="lead">{description}</p>
+                </div>
+                <div className={className}>{children}</div>
+            </div>
+            <footer className="footer bg-dark mt-auto py3">
+                <div className="container-fluid bg-success text-white text-center py-3">
+                    <h4>if you got any questions feel free to reach out</h4>
+                    <button className="btn btn-warning btn-lg">Contact us</button>
+                </div>
+                <div className="container">
+                    <span className="text-muted ">Amazing <span className="text-white">MERN</span> Bootcamp</span>
+                </div>
+            </footer>
+
+        </div>
+    )
+
+
+export default  Base;
--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import "../styles.css"
 import {API} from "../backend"
+import Base from './Base';
 
 export default function Home() {
     console.log("APP IS" , API);
     return (
-        <div>
-            <h1 className="text-white">Hello frontend {API}</h1>
-        </div>
+        <Base title="Home Page">
+            <div className="row">
+                <div className="col-4">
+                    <button className="btn btn-success">Test</button>
+                </div>
+                <div className="col-4">
+                    <button className="btn btn-success">Test</button>
+                </div>
+                <div className="col-4">
+                    <button className="btn btn-success">Test</button>
+                </div>
+            </div>
+        </Base>
     )
 }
