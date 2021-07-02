dbd4a21f04d0b520ae80f93d42b631dee065dde9
Nikhil <nikhilpawar982001@gmail.com>
2021-07-02 12:03:24 +0530

    13.5

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "../styles.css"
+import {API} from "../backend"
 
 export default function Home() {
+    console.log("APP IS" , API);
     return (
         <div>
-            <h1 className="text-white">Hello frontend</h1>
+            <h1 className="text-white">Hello frontend {API}</h1>
         </div>
     )
 }
