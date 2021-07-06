7e80c5c552f10094d0eacb0f181ef3ba877a05bb
Nikhil <nikhilpawar982001@gmail.com>
2021-07-06 23:52:57 +0530

    14.8

--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -1,45 +1,108 @@
-import React, {useSate} from "react";
+import React, { useSate } from "react";
 import Base from "../core/Base";
-import {Link} from "react-router-dom"
+import { Link, Redirect } from "react-router-dom";
 
+import {} from "../auth/helper";
+import { signin, authenticate, isAuthenticated } from "../auth/helper";
 
-const Signin = () =>{
+const Signin = () => {
+  const [values, setValues] = useSate({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    didRedirect: false,
+  });
 
-    const signInForm = () => {
-        return (
-          <div className="row">
-            <div className="col-md-6 offset-sm-3 text-left">
-              <form action="">
-    
-                
-                  
-                <div className="form-group">
-                  <label htmlFor="" className="text-light">
-                    Email
-                  </label>
-                  <input className="form-control" type="email" />
-                </div>
-    
-                <div className="form-group">
-                  <label htmlFor="" className="text-light">
-                    password 
-                  </label>
-                  <input className="form-control" type="password" />
-                </div>
-    
-                <button className="btn btn-success btn-block">Submit</button>
-    
-              </form>
-            </div>
-          </div>
-        );
+  const { email, password, error, loading, didRedirect } = values;
+  const { user } = isAuthenticated();
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const onSubmit = event =>{
+    event.preventDefault();
+    setValues({...values,error: false, loading:true })
+    signin({email,password})
+    .then(data=>{
+      if (data.error) {
+        setValues({...values,error: data.error, loading:false })
+      } else{
+        authenticate(data, ()=>{
+          setValues({
+            ...values,
+            didRedirect:true
+
+          })
+        })
       }
-
+    })
+    .catch(console.log("sigin request failed"))
+  
+  }
+  const successMessage = () => {
     return (
-        <Base title=" Sign In Page" description="A page for signing In">
-        {signInForm()}
-        </Base>
-    )
-}
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-success"
+            style={{ display: success ? "" : "none" }}
+          >
+            New account was created successfully. Please{" "}
+            <Link to="/signin">login here</Link>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const errorMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+          >
+            {error}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const signInForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form action="">
+            <div className="form-group">
+              <label htmlFor="" className="text-light">
+                Email
+              </label>
+              <input onChange={handleChange("email")} value={email} className="form-control" type="email" />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="" className="text-light">
+                password
+              </label>
+              <input  onChange={handleChange("password")} value={password} className="form-control" type="password" />
+            </div>
+
+            <button onClick={onSubmit} className="btn btn-success btn-block">Submit</button>
+          </form>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <Base title=" Sign In Page" description="A page for signing In">
+      {signInForm()}
+    </Base>
+  );
+};
 
 export default Signin;
