06f1d3ffd4dcaeab4dba287493bf89fd69e9142e
Nikhil <nikhilpawar982001@gmail.com>
2021-07-13 18:36:00 +0530

    Added Category  routes in admin panel

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -1,30 +1,28 @@
-import React from 'react'
-import {BrowserRouter, Switch, Route} from "react-router-dom"
-import Home from './core/Home'
-import Signin from './user/Signin'
-import Signup from './user/Signup'
-import AdminRoute from "./auth/helper/AdminRoutes"
-import PrivateRoute from "./auth/helper/PrivateRoutes"
-import UserDashboard from "./user/UserDashBoard"
-import AdminDashboard from "./user/AdminDashBoard"
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./core/Home";
+import Signin from "./user/Signin";
+import Signup from "./user/Signup";
+import AdminRoute from "./auth/helper/AdminRoutes";
+import PrivateRoute from "./auth/helper/PrivateRoutes";
+import UserDashboard from "./user/UserDashBoard";
+import AdminDashboard from "./user/AdminDashBoard";
+import AddCategory from "./admin/AddCategory";
 
-
-
-const Routes =() => {
+const Routes = () => {
   return (
     <BrowserRouter>
-    <Switch>
-      <Route path="/" exact component = {Home}/>
-      <Route path="/signup" exact component = {Signup}/>
-      <Route path="/signin" exact component = {Signin}/>
-      <PrivateRoute path="/user/dashboard" exact component = {UserDashboard} />
-      <AdminRoute path="/admin/dashboard" exact component = {AdminDashboard} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signup" exact component={Signup} />
+        <Route path="/signin" exact component={Signin} />
+        <PrivateRoute path="/user/dashboard" exact component={UserDashboard} />
+        <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
 
-
-    </Switch>
+        <AdminRoute path="/admin/create/category" exact component={AddCategory}/>
+      </Switch>
     </BrowserRouter>
-  )
-}
-
+  );
+};
 
 export default Routes;
--- projfrontend/src/user/AdminDashBoard.js
+++ projfrontend/src/user/AdminDashBoard.js
@@ -1,12 +1,61 @@
-import React from 'react'
- import Base from "../core/Base"
+import React from "react";
+import { isAuthenticated } from "../auth/helper";
+import Base from "../core/Base";
+import { Link } from "react-router-dom";
 
-const  AdminDashBoard =()=> {
+const AdminDashBoard = () => {
+  const {
+    user: { name, email, role },
+  } = isAuthenticated();
+
+  const adminLeftSide = () => {
     return (
-        <Base title="AdminDashBoard page">
-        <h1>This is AdminDashBoard page</h1>
-        </Base>
-    )
-}
+      <div className="card">
+        <h4 className="card-header bg-dark text-white">Admin Navigation</h4>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <Link to="/admin/create/category" className="nav-link text-success"> Create Categories</Link>
+            <Link to="/admin/create/products" className="nav-link text-success"> Create Product</Link>
+            <Link to="/products" className="nav-link text-success"> Manage Products</Link>
+            <Link to="/orders" className="nav-link text-success"> Manage Orders</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  const adminRightSide = () => {
+    return (
+        <div className="card mb-4">
+            <h4 className="card-header">Admin Information</h4>
+            <ul className="list-group">
+                <li className="list-group-item">
+                    <span className="badge bg-success mr-2 ">Name:</span> {name} 
+                </li>
+                <li className="list-group-item">
+                    <span className="badge bg-success mr-2 ">Email:</span> {email}
+                </li>
+                <li className="list-group-item">
+                    <span className="badge bg-danger">Admin area</span>
+                </li>
+            </ul>
+        </div>
+    );
+  };
+
+  return (
+    <Base
+      title="Welcome to admin area"
+      description="Manage all of your products here"
+      className="container bg-success p-4"
+    >
+      <div className="row">
+        <div className="col-3">{adminLeftSide()}</div>
+
+        <div className="col-9">{adminRightSide()}</div>
+      </div>
+    </Base>
+  );
+};
 
 export default AdminDashBoard;
