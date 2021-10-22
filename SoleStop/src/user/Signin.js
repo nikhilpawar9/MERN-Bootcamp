8ee0a62b7a321ee72f765abb61e8761d61d3bb08
Nikhil <nikhilpawar982001@gmail.com>
2021-10-22 17:55:52 +0530

    More Fixes

--- SoleStop/src/user/Signin.js
+++ SoleStop/src/user/Signin.js
@@ -6,8 +6,8 @@ import { signin, authenticate, isAuthenticated } from "../auth/helper";
 
 const Signin = () => {
   const [values, setValues] = useState({
-    email: "admin@hitesh.com",
-    password: "123456",
+    email: "",
+    password: "",
     error: "",
     loading: false,
     didRedirect: false
